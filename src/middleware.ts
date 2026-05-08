349e431180bae53c66408dbcb266468fee873f65
djojo <djojo@example.com>
2026-05-08 17:16:58 -0500

    fix(clerk): correct matcher for middleware to intercept API routes and fix auth error

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,15 @@ import { NextRequest } from "next/server";
 // This matcher makes all routes public by default unless protected.
 const isPublicRoute = createRouteMatcher(['(.*)']);
 
-export default clerkMiddleware((auth, request: NextRequest) => {
-  return updateSession(request);
+export default clerkMiddleware(async (auth, request: NextRequest) => {
+  return await updateSession(request);
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
